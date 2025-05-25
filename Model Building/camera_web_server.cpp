ce5b47f6186a4f7bde2d56f3e560a38e15a3bf2b
ramavataryadav <yadav.avtar97@gmail.com>
2025-05-25 16:41:33 +0530

    Update camera_web_server.cpp

--- Model Building/camera_web_server.cpp
+++ Model Building/camera_web_server.cpp
@@ -1,1 +1,18 @@
+#include <WebServer.h>
+WebServer server(80);
 
+void handleCapture() {
+  camera_fb_t *fb = esp_camera_fb_get();
+  if (!fb) {
+    server.send(500, "text/plain", "Camera capture failed");
+    return;
+  }
+  server.send_P(200, "image/jpeg", (char *)fb->buf, fb->len);
+  esp_camera_fb_return(fb);
+}
+
+void startCameraServer() {
+  server.on("/", HTTP_GET, handleCapture);
+  server.begin();
+  Serial.println("HTTP server started");
+}
